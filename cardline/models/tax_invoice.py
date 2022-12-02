5f33ba8c47e958887287a5f4ebe00ba352d06b94
meghanapmcpl <meghana@primemindsconsulting.com>
2022-12-02 11:12:28 +0530

    invoice modified

--- cardline/models/tax_invoice.py
+++ cardline/models/tax_invoice.py
@@ -10,6 +10,24 @@ class TaxInvoiceReportInherit(models.Model):
     exchange_rate = fields.Float(string="Exchange Rate")
     currency = fields.Char(string="Currency")
 
+    def total(self):
+        total = 0
+        for rec in self:
+            for line in rec.invoice_line_ids:
+                total = total + (line.quantity * line.price_unit)
+                print(total)
+        return total
+
+    def discount_calculate(self):
+        discount = 0
+        for rec in self:
+            for line in rec.invoice_line_ids:
+                # print("LLLLLLLLLL",line)
+                print("line dis",line.discount)
+                discount = discount + line.discount
+                # print(discount)
+        return discount
+
 class InvoiceFormOrderLinesInherit(models.Model):
     _inherit = 'account.move.line'
 
