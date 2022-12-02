0d7713682e6328991294068ee3f83b5b5fb48c3f
meghanapmcpl <meghana@primemindsconsulting.com>
2022-12-02 16:53:13 +0530

    proforma modified

--- report_inventory/models/quotation_changes.py
+++ report_inventory/models/quotation_changes.py
@@ -8,19 +8,28 @@ class StockPicking(models.Model):
 
     customer_reference = fields.Char(string="Customer Reference")
 
-    def calculations(self):
+    def calculations_total(self):
         for rec in self:
-            # for lines in rec.order_lines:
-            result = rec.tax_totals['formatted_amount_total']
-            length = len(result)
-            # print("lengthhhhhhh",length)
-            slice_res = result[:length - 4]
-            # print("sliced result",slice_res)
-            # print("resulttttttttttttttttt",result)
-            final = slice_res +" "+"AED"
-            # print("finalllllllllll",final)
-            return final
+            format_total = rec.tax_totals['formatted_amount_total']
+            length_total = len(format_total)
+            sliced_res_total = format_total[:length_total - 4]
+            final_total = sliced_res_total +" "+"AED"
+            return final_total
 
+    def calculations_vat(self):
+        for rec in self:
+            format_vat = rec.tax_totals['groups_by_subtotal']['Untaxed Amount'][0]['formatted_tax_group_amount']
+            length_vat = len(format_vat)
+            slice_res_vat = format_vat[:length_vat - 4]
+            final_vat = slice_res_vat +" "+"AED"
+            return final_vat
 
+    def calculations_untaxed_amt(self):
+        for rec in self:
+            format_untaxed_amt = rec.tax_totals['formatted_amount_untaxed']
+            length_untaxed_amt = len(format_untaxed_amt)
+            slice_res_untaxed_amt = format_untaxed_amt[:length_untaxed_amt - 4]
+            final_untaxed_amt = slice_res_untaxed_amt +" "+"AED"
+            return final_untaxed_amt
 
 
