065223f281af1f7cc7c03e814fecbf61dcadf96a
meghanapmcpl <meghana@primemindsconsulting.com>
2022-12-08 12:38:08 +0530

    report_inventory modified

--- report_inventory/models/quotation_changes.py
+++ report_inventory/models/quotation_changes.py
@@ -21,30 +21,30 @@ class StockPicking(models.Model):
         amount_return = sliced_amount +" "
         return amount_return
 
-    def calculations_total(self,):
-        for rec in self:
-            format_total = rec.tax_totals['formatted_amount_total']
-            print("HGHHHHHHHHHHHHH",format_total)
-            length_total = len(format_total)
-            sliced_res_total = format_total[:length_total - 4]
-            print("SSSSSSSSSSS",sliced_res_total)
-            final_total = sliced_res_total+" "
-            return final_total
+    # def calculations_total(self,):
+    #     for rec in self:
+    #         format_total = rec.tax_totals['formatted_amount_total']
+    #         print("HGHHHHHHHHHHHHH",format_total)
+    #         length_total = len(format_total)
+    #         sliced_res_total = format_total[:length_total - 4]
+    #         print("SSSSSSSSSSS",sliced_res_total)
+    #         final_total = sliced_res_total+" "
+    #         return final_total
 
-    def calculations_vat(self):
-        for rec in self:
-            format_vat = rec.tax_totals['groups_by_subtotal']['Untaxed Amount'][0]['formatted_tax_group_amount']
-            length_vat = len(format_vat)
-            slice_res_vat = format_vat[:length_vat - 4]
-            final_vat = slice_res_vat +" "
-            return final_vat
+    # def calculations_vat(self):
+    #     for rec in self:
+    #         format_vat = rec.tax_totals['groups_by_subtotal']['Untaxed Amount'][0]['formatted_tax_group_amount']
+    #         length_vat = len(format_vat)
+    #         slice_res_vat = format_vat[:length_vat - 4]
+    #         final_vat = slice_res_vat +" "
+    #         return final_vat
 
-    def calculations_untaxed_amt(self):
-        for rec in self:
-            format_untaxed_amt = rec.tax_totals['formatted_amount_untaxed']
-            length_untaxed_amt = len(format_untaxed_amt)
-            slice_res_untaxed_amt = format_untaxed_amt[:length_untaxed_amt - 4]
-            final_untaxed_amt = slice_res_untaxed_amt +" "
-            return final_untaxed_amt
+    # def calculations_untaxed_amt(self):
+    #     for rec in self:
+    #         format_untaxed_amt = rec.tax_totals['formatted_amount_untaxed']
+    #         length_untaxed_amt = len(format_untaxed_amt)
+    #         slice_res_untaxed_amt = format_untaxed_amt[:length_untaxed_amt - 4]
+    #         final_untaxed_amt = slice_res_untaxed_amt +" "
+    #         return final_untaxed_amt
 
 
